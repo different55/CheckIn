6d83f4cdd9283ea8507e86fd7d6984ba1c21b0f2
Diff <diff@tilde.town>
2020-11-12 22:06:19 +0000

    Basic functionality

---
+++ README.md
@@ -0,0 +1,7 @@
+# Venture
+
+A way to let other people know what the heck you're up to.
+
+## Usage:
+
+`venture "<username>[message]"`
--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"path"
+	"os"
+	"os/user"
+	"io"
+	"io/ioutil"
+	"bufio"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	curUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("What's ~%v been up to?\n~%v", curUser.Username, curUser.Username)
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+
+	input = "~"+curUser.Username+input
+	
+	outputPath := path.Join(curUser.HomeDir, ".venture")
+	err = ioutil.WriteFile(outputPath, []byte(input), 0644)
+	if err != nil {
+		panic(err)
+	}
 }
