557558cc88cd1345fb5d2db6e1e47c091a668a68
Diff <diff@tilde.town>
2020-11-13 03:11:40 +0000

    Add some comments

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ func main() {
 		panic(err)
 	}
 
+	// User status is written to ~/.venture
 	outputPath := path.Join(curUser.HomeDir, ".venture")
 
+	// Prompt user for input
 	fmt.Printf("What's ~%v been up to?\n~%v", curUser.Username, curUser.Username)
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
@@ -25,6 +27,7 @@ func main() {
 		panic(err)
 	}
 
+	// Remove file on blank input
 	if len(input) < 1 {
 		err = os.Remove(outputPath)
 		if err != nil {
@@ -33,8 +36,10 @@ func main() {
 		return
 	}
 
+	// Prepend status with user's name
 	input = "~"+curUser.Username+input
-	
+
+	// Write file and create if it doesn't exist as world-readable.
 	err = ioutil.WriteFile(outputPath, []byte(input), 0644)
 	if err != nil {
 		panic(err)
